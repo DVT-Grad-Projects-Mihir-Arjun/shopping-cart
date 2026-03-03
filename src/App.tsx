739f1ce8f181476eed8473cfe355070f9ba17820
Mihir-Arjun-dvt <MArjun@dvtsoftware.com>
2026-03-03 14:23:44 +0200

    basic card layout is now working with data from API and only using useContext(no props)

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
+import ProductCards from './components/ProductCards.tsx';
 import './index.css'
+import { ProductContext } from './contexts/ProductContext.tsx';
 
 const products = await Promise.all(
   [1, 2, 3, 4].map(id =>
-    fetch(`https://fakestoreapi.com/products/${id}`).then(res => res.json())
+    fetch(`https://fakestoreapi.com/products/${id}`).then(response => response.json())
   )
 );
 
 function App() {
   return (
     <>
-      {products.map(product => (
-        <div key={product.id}>{product.title}</div>
-      ))}
+      <ProductContext value={products}>
+        <ProductCards />
+      </ProductContext>
     </>
   )
 }
---
+++ src/components/ProductCards.tsx
@@ -0,0 +1,29 @@
+import { useContext } from 'react';
+import { ProductContext } from '../contexts/ProductContext.tsx';
+
+export default function ProductCards() {
+    const products = useContext(ProductContext);
+
+    return (
+        <div className="flex flex-wrap justify-center gap-4 p-4">
+            {products.map(product => (
+                <div key={product.id} className="card bg-base-100 w-96 shadow-sm">
+                    <figure>
+                        <img src={product.image} alt={product.title} className='h-100 w-96 p-4' />
+                    </figure>
+                    <div className="card-body">
+                        <h2 className="card-title">{product.title}</h2>
+                        <div className="flex items-center justify-between">
+                            <p className='text-xl font-bold'>${product.price}</p>
+                            <div className="badge badge-accent">{product.category.toUpperCase()}</div>
+                        </div>
+                        <p>{product.description}</p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Buy Now</button>
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+}
